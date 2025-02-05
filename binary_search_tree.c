d7f5d871c7c7e06f1976bd1bf304abdac1b65d35
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2025-02-05 15:45:16 +0300

    depth-first-search

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -15,9 +15,78 @@ Tree *create_tree()
     Tree *tree = (Tree*)malloc(sizeof(Tree));
     if(tree==NULL){
         printf("Memory allocation failed\n");
-        return;
+        return NULL;
     }
     tree->root = NULL;
 tree->size=0;
 return tree;
+}
+void insert_node(Tree *tree,int data){
+Node *new_node=(Node*)malloc(sizeof(Node));
+if(new_node==NULL){
+    printf("Memory allocation failed\n");
+    return;
+};
+new_node->data=data;
+new_node->left=NULL;
+new_node->right=NULL;
+if(tree->root==NULL){
+    tree->root=new_node;
+}
+else{
+insert(new_node,tree->root);
+}
+tree->size++;
+}
+void insert(Node *node,Node *root){
+if(root==NULL)
+{
+    return;
+}
+if(node->data>root->data){
+if(root->right==NULL){
+    root->right=node;
+}
+else{
+    insert(node,root->right);
+}
+}
+else if(node->data<root->data){
+if(root->left==NULL){
+root->left=node;
+}
+else{
+    insert(node,root->left);
+}
+}
+}
+void preorder(Node *node){
+ if(node==NULL){
+return;
+ }   
+ else{
+    printf("%d ",node->data);
+    preorder(node->left);
+    preorder(node->right);
+ }
+}
+void inorder(Node *node){
+ if(node==NULL){
+return;
+ }   
+ else{
+    inorder(node->left);
+    printf("%d ",node->data);
+    inorder(node->right);
+ }
+}
+void postorder(Node *node){
+ if(node==NULL){
+return;
+ }   
+ else{
+    postorder(node->left);
+    postrder(node->right);
+    printf("%d ",node->data);
+ }
 }
